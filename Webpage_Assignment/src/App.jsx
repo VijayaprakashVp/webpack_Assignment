b9a3db378313b446e9b535a43ce5dc9ae956c46d
VijayaprakashVp <prakashvijay905@gmail.com>
2022-03-08 18:57:42 +0530

    finished

--- Webpage_Assignment/src/App.jsx
+++ Webpage_Assignment/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./Logo.png";
 
 function App() {
   const [note, setNote] = useState("");
@@ -11,18 +12,28 @@ function App() {
 
   return (
     <div className="App">
-      <img src="" alt="Logo" />
-      <input
-        onChange={(e) => setNote(e.target.value)}
-        type="text"
-        placeholder="Add notes here"
-        id="input_box"
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <div>
-        {note_taking.map((e) => {
-          return <p>{e}</p>;
-        })}
+      <div className="div">
+        <h1>Note Taking App</h1>
+        <div>
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo-700x394.png"
+            alt="Logo"
+          />
+        </div>
+        <div>
+          <input
+            onChange={(e) => setNote(e.target.value)}
+            type="text"
+            placeholder="Add notes here"
+            id="input_box"
+          />
+        </div>
+        <button onClick={handleSubmit}>Submit</button>
+        <div>
+          {note_taking.map((e) => {
+            return <p>{e}</p>;
+          })}
+        </div>
       </div>
     </div>
   );
